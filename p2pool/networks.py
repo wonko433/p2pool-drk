ba91170c987c566238ffec8557f9846bc9f064e3
chaeplin <chaeplin@gmail.com>
2014-04-09 19:28:20 +0900

    change BOOTSTRAP_ADDRS

--- p2pool/networks.py
+++ p2pool/networks.py
@@ -25,7 +25,7 @@ nets = dict(
         MAX_TARGET=2**256//2**20 - 1,
         PERSIST=False,
         WORKER_PORT=7903,
-        BOOTSTRAP_ADDRS='p2phash.com asia02.poolhash.org asia01.poolhash.org 157.56.161.11 54.186.8.140 62.141.39.175 mightypool.net 85.131.127.26 213.229.88.102 cryptohasher.net darkcoin.fr'.split(' '),
+        BOOTSTRAP_ADDRS='asia02.poolhash.org asia01.poolhash.org q30.qhor.net poulpe.nirnroot.com drk.p2pool.n00bsys0p.co.uk drk.altmine.net darkcoin.kicks-ass.net darkcoin.fr cryptohasher.net coinminer.net'.split(' '),
         ANNOUNCE_CHANNEL='#p2pool-drk',
         VERSION_CHECK=lambda v: True,
     ),
