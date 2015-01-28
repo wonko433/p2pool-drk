cfbfb8ad684950d266c602562681bf2efce88ef1
vertoe <vertoe@qhor.net>
2015-01-28 13:34:34 +0100

    Fix testnet explorer.

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -53,9 +53,9 @@ nets = dict(
         BLOCK_PERIOD=150, # s
         SYMBOL='tDRK',
         CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Darkcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Darkcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.darkcoin'), 'darkcoin.conf'),
-        BLOCK_EXPLORER_URL_PREFIX='http://test.explorer.darkcoin.io/block/',
-        ADDRESS_EXPLORER_URL_PREFIX='http://test.explorer.darkcoin.io/address/',
-        TX_EXPLORER_URL_PREFIX='http://test.explorer.darkcoin.io/tx/',
+        BLOCK_EXPLORER_URL_PREFIX='http://test.explorer.darkcoin.qa/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://test.explorer.darkcoin.qa/address/',
+        TX_EXPLORER_URL_PREFIX='http://test.explorer.darkcoin.qa/tx/',
         SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**20 - 1),
         DUMB_SCRYPT_DIFF=1,
         DUST_THRESHOLD=0.001e8,
